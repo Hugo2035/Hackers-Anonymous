d165543a213893b113502950a11c208b722f4c8a
ssilen1 <78632034+ssilen1@users.noreply.github.com>
2021-03-18 22:03:03 -0500

    Sheldon Silen
    Class layout for "Classes" and "Study Buddy"

---
+++ Classes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Classes
+{
+	public int Class_ID;
+	public string Class_Name;
+	private string Teacher;
+
+	public Classes()
+	{
+        public static void getAssignments()
+        {
+
+        }
+
+        public static void getDueDates()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
