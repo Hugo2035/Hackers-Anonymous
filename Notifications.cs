06573ee4e667d05766c19dfdea8e1c7c000b7713
KSchwarzlose95 <kmschwarzl@eagles.usi.edu>
2021-03-18 22:24:06 -0500

    Create Notifications.cs

---
+++ Notifications.cs
@@ -0,0 +1,21 @@
+namespace Classes
+{
+    public class Notifications
+    {
+        public string class_notification;
+        public string user_notification;
+
+        public Notifications()
+        {
+            static void setNotifications()
+            {
+
+            }
+
+            static void customNotification()
+            {
+
+            }
+        }
+    }
+}
