347c95cc4f2ec6604ceaaa25d7942f3aac93f1cf
KSchwarzlose95 <kmschwarzl@eagles.usi.edu>
2021-03-18 22:24:20 -0500

    Create Calendar.cs

---
+++ Calendar.cs
@@ -0,0 +1,18 @@
+namespace Classes
+{
+    public class Calendar
+    {
+        public string date_display;
+        public string due_date_display;
+
+        public Calendar()
+        {
+            static void classDisplay()
+            {
+
+            }
+        }
+
+    }
+
+}
